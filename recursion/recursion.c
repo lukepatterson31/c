b583d0fbeed6ba5b13f399695cb2c2fde395cf83
Luke Patterson <lukepatterson31@gmail.com>
2024-02-12 16:40:22 +0000

    clean up

--- recursion/recursion.c
+++ recursion/recursion.c
@@ -7,20 +7,20 @@ int main(int argc, char const *argv[])
 {
     if(argc > 1)
     {
-        // next char in array after parsed int
         char *output;
-        // parse argument to int (string or char to parse, next array item, base)
-        int num = strtol(argv[1], &output, 10);
-        int x = factorial(num);
-        printf("%i! = %i\n", num, x);
+        // parse argument char to int (char to parse, outvar for next item in array after parsed int, number base)
+        int num = strtoi(argv[1], &output, 10);
+        printf("%i! = %i\n", num, factorial(num));
         exit(EXIT_SUCCESS);
     }
+
     printf("Error: missing argument\n");
     exit(EXIT_FAILURE);
 }
 
 
-int factorial(int x){
+int factorial(int x)
+{
     if(x > 1)
     {
         return x * factorial(x-1);
